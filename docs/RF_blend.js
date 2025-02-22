5bc1db97fbb6b79325646c16627f7e2957f41cae
Stephen McCabe <mccabesp@hotmail.com>
2025-02-22 10:46:12 +0000

    Reformatted and cleaned up

--- docs/RF_blend.js
+++ docs/RF_blend.js
@@ -1,4 +1,3 @@
-
 var tiles = 9    // number of tiles
 var total = 25   // total number of small pictures
 var totalfs = 10 // total number of full size pictures
@@ -13,34 +12,34 @@ preLoad[0] = new Image;
 preLoad[0].src = "images/home/0.jpg";
 
 // shuffle images
-for (i=1; i<=total; i++) {
+for (i = 1; i <= total; i++) {
     mix = Math.floor(Math.random() * i) + 1;
     pic[i] = pic[mix];
     pic[mix] = i;
 }
 
 // preload images, in the order they will be displayed
-for (i=1; i<=total; i++) {
+for (i = 1; i <= total; i++) {
     preLoad[i] = new Image;
-    preLoad[i].src = "images/home/"+pic[i]+".jpg";
+    preLoad[i].src = "images/home/" + pic[i] + ".jpg";
 }
 
 // shuffle full size images
-for (i=1; i<=totalfs; i++) {
-	mix = Math.floor(Math.random() * i) + 1;
+for (i = 1; i <= totalfs; i++) {
+    mix = Math.floor(Math.random() * i) + 1;
     picfs[i] = picfs[mix];
     picfs[mix] = i;
 }
 
 // preload full size images, in the order they will be displayed
-for (i=1; i<=totalfs; i++) {
-	for (j=1; j<=9; j++) {
-    	preLoadfs[j+((i-1)*9)] = new Image;
-    	preLoadfs[j+((i-1)*9)].src = "images/home/whole/"+picfs[i]+"_"+j+".jpg";
-	}
+for (i = 1; i <= totalfs; i++) {
+    for (j = 1; j <= 9; j++) {
+        preLoadfs[j + ((i - 1) * 9)] = new Image;
+        preLoadfs[j + ((i - 1) * 9)].src = "images/home/whole/" + picfs[i] + "_" + j + ".jpg";
+    }
 }
 
-var t=0, p=1, fs=0, fno, timeout=0, pfs=1
+var t = 0, p = 1, fs = 0, fno, timeout = 0, pfs = 1
 var tile = new Array(tiles)
 
 // shuffle tile sequence
@@ -51,42 +50,49 @@ for (i = 0; i < tiles; i++) {
 }
 
 function blendit() {
-    
-    if (t==tiles) {t = 0; speed = 500}			// Slow down after all tiles filled
-	if (timeout) {timeout=0; speed = 500}		// Speed up after large image pause
-	
-	if (document.all) {
-    	document.images["tile"+tile[t]].style.filter="blendTrans(duration=2)";
-    	document.images["tile"+tile[t]].filters.blendTrans.apply();
-    }
-	
-    if (p==total) {					// At the end of a cycle
-		fs++;
-		fno=(pfs*9)+tile[t]-8;
-		document.images["tile"+tile[t]].src=preLoadfs[fno].src;
-		if (fs==tiles) {
-	 		if (pfs==totalfs) {pfs=0}
-			pfs++;
-			p=1;
-			fs=0;
-			speed=3000;
-			timeout=1;
-	 	 }
-    }
-    else {
-		fno=p;
-		p++;
-		document.images["tile"+tile[t]].src=preLoad[fno].src;
-    }
-   
+
+    if (t == tiles) {
+        t = 0;
+        speed = 500
+    }			// Slow down after all tiles filled
+    if (timeout) {
+        timeout = 0;
+        speed = 500
+    }		// Speed up after large image pause
+
     if (document.all) {
-    	document.images["tile"+tile[t]].filters.blendTrans.play();
+        document.images["tile" + tile[t]].style.filter = "blendTrans(duration=2)";
+        document.images["tile" + tile[t]].filters.blendTrans.apply();
     }
-    
-	t++
-    setTimeout("blendit()",speed)
+
+    if (p == total) {					// At the end of a cycle
+        fs++;
+        fno = (pfs * 9) + tile[t] - 8;
+        document.images["tile" + tile[t]].src = preLoadfs[fno].src;
+        if (fs == tiles) {
+            if (pfs == totalfs) {
+                pfs = 0
+            }
+            pfs++;
+            p = 1;
+            fs = 0;
+            speed = 3000;
+            timeout = 1;
+        }
+    } else {
+        fno = p;
+        p++;
+        document.images["tile" + tile[t]].src = preLoad[fno].src;
+    }
+
+    if (document.all) {
+        document.images["tile" + tile[t]].filters.blendTrans.play();
+    }
+
+    t++
+    setTimeout("blendit()", speed)
 }
 
 function delayblend() {
-	setTimeout("blendit()",1000)
+    setTimeout("blendit()", 1000)
 }
--- docs/scrollbar.js
+++ docs/scrollbar.js
@@ -1,10 +1,10 @@
-browser_version= parseInt(navigator.appVersion);
+browser_version = parseInt(navigator.appVersion);
 browser_type = navigator.appName;
 if (browser_type == "Microsoft Internet Explorer" && (browser_version >= 4)) {
-document.write('<style type="text/css">');
-document.write('html ');
-document.write('{scrollbar-3dlight-color:#999999;scrollbar-arrow-color:#000000;scrollbar-base-color:#000000;');
-document.write('scrollbar-darkshadow-color:#999999;scrollbar-face-color:#000000;');
-document.write('scrollbar-highlight-color:#000000;scrollbar-shadow-color:#000000;}');
-document.write('</style>');
+    document.write('<style type="text/css">');
+    document.write('html ');
+    document.write('{scrollbar-3dlight-color:#999999;scrollbar-arrow-color:#000000;scrollbar-base-color:#000000;');
+    document.write('scrollbar-darkshadow-color:#999999;scrollbar-face-color:#000000;');
+    document.write('scrollbar-highlight-color:#000000;scrollbar-shadow-color:#000000;}');
+    document.write('</style>');
 }
